f11add114a73296de5a7aa71affb46a55568e92f
sasaya7016 <sasaya7016@outlook.jp>
2020-06-29 06:07:59 +0900

    deploy

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
             @include('error_card_list')
 
             <div class="card-text">
-              {{--ここから--}}
+             
               <form method="POST" action="{{ route('register') }}">
                 @csrf
                 <div class="md-form">
@@ -36,7 +36,7 @@
                 </div>
                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ユーザー登録</button>
               </form>
-              {{--ここまで--}}
+          
 
               <div class="mt-0">
                 <a href="{{ route('login') }}" class="card-text">ログインはこちら</a>
--- resources/views/users/tabs.blade.php
+++ resources/views/users/tabs.blade.php
@@ -2,7 +2,7 @@
   <li class="nav-item">
     <a class="nav-link text-muted {{ $hasArticles ? 'active' : '' }}"
        href="{{ route('users.show', ['name' => $user->name]) }}">
-      記事
+      ツイート
     </a>
   </li>
   <li class="nav-item">
